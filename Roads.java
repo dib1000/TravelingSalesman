832c0ea5fe03c8960dd1caee6b7bc1ad7f59e40d
ellakrechmer <ekrechmer20@stuy.edu>
2021-01-19 16:34:47 -0500

    removed some stuff and filled in other half of the array

--- Roads.java
+++ Roads.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class Roads{
 
-  public static void print2D(int mat[][]){
-    for (int i = 0; i < mat.length; i++){
-      for (int j = 0; j < mat[i].length; j++){
-        System.out.print(mat[i][j] + " ");
-      }
-      System.out.println("");
-    }
-  }
+  // public static void print2D(int mat[][]){
+  //   for (int i = 0; i < mat.length; i++){
+  //     for (int j = 0; j < mat[i].length; j++){
+  //       System.out.print(mat[i][j] + " ");
+  //     }
+  //     System.out.println("");
+  //   }
+  // }
 
   public static void main(String[] args) {
     ArrayList<String> cities = new ArrayList<String>();
@@ -38,10 +38,10 @@ public class Roads{
         line.next();
         int distance = line.nextInt();
         distances[cities.indexOf(city1)][cities.indexOf(city2)] = distance;
+        distances[cities.indexOf(city2)][cities.indexOf(city1)] = distance;
       }
     }
     System.out.println(cities);
-    print2D(distances);
   }
 
 }
